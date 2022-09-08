5f12803a605ed9546cf43d5fa9f33b8c5d5ae670
Daniel McKnight <daniel@neon.ai>
2022-09-08 09:30:09 -0700

    Update package name in scripts and workflows Add version.py and update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,40 @@ from setuptools import setup
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'ovos_PHAL_plugin_mk2', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
 def required(requirements_file):
     """ Read requirements file and remove comments and empty lines. """
     with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
@@ -17,13 +51,14 @@ def required(requirements_file):
 PLUGIN_ENTRY_POINT = 'ovos-PHAL-plugin-system=ovos_PHAL_plugin_system:SystemEvents'
 setup(
     name='ovos-PHAL-plugin-system',
-    version='0.0.1',
+    version=get_version(),
     description='A plugin for OpenVoiceOS hardware abstraction layer',
     url='https://github.com/OpenVoiceOS/ovos-PHAL-plugin-system',
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
     license='Apache-2.0',
     packages=['ovos_PHAL_plugin_system'],
+    package_data={'': package_files('ovos_PHAL_plugin_system')},
     install_requires=required("requirements.txt"),
     zip_safe=True,
     classifiers=[
