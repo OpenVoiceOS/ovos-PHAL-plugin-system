0c1b8b0526a00b7952ad52c40e4a2c7f7ea2067d
Daniel McKnight <daniel@neongecko.com>
2023-06-21 12:19:45 -0700

    Update automation to shared actions

--- setup.py
+++ setup.py
@@ -49,11 +49,20 @@ def required(requirements_file):
         return [pkg for pkg in requirements
                 if pkg.strip() and not pkg.startswith("#")]
 
+
+def get_description():
+    with open(os.path.join(BASEDIR, "readme.md"), "r") as f:
+        long_description = f.read()
+    return long_description
+
+
 PLUGIN_ENTRY_POINT = 'ovos-PHAL-plugin-system=ovos_PHAL_plugin_system:SystemEvents'
 setup(
     name='ovos-PHAL-plugin-system',
     version=get_version(),
     description='A plugin for OpenVoiceOS hardware abstraction layer',
+    long_description=get_description(),
+    long_description_content_type="text/markdown",
     url='https://github.com/OpenVoiceOS/ovos-PHAL-plugin-system',
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
